aaa682bf14e05836945a391ba422aa2b945eac69
ishenaly007 <ishennarkozuev07@gmail.com>
2024-09-25 16:01:27 +0600

    try thymeleaf, create users page

--- src/main/java/com/abit/spring/http/controller/UserController.java
+++ src/main/java/com/abit/spring/http/controller/UserController.java
@@ -2,6 +2,8 @@ package com.abit.spring.http.controller;
 
 import com.abit.spring.dto.UserCreateEditDto;
 import com.abit.spring.dto.UserReadDto;
+import com.abit.spring.entity.Role;
+import com.abit.spring.service.CompanyService;
 import com.abit.spring.service.UserService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
@@ -9,6 +11,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.server.ResponseStatusException;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 @Controller
 @RequestMapping("/users")
@@ -16,6 +19,7 @@ import org.springframework.web.server.ResponseStatusException;
 public class UserController {
 
     private final UserService userService;
+    private final CompanyService companyService;
 
     @GetMapping
     public String findAllUsers(Model model) {
@@ -24,17 +28,33 @@ public class UserController {
         return "user/users";
     }
 
+    @GetMapping("/registration")
+    public String registration(Model model, @ModelAttribute("user") UserCreateEditDto user) {
+        model.addAttribute("user", user);
+        model.addAttribute("roles", Role.values());
+        model.addAttribute("companies", companyService.findAll());
+        return "user/registration";
+    }
+
     @GetMapping("/{id}")
     public String findUserById(@PathVariable("id") Integer id, Model model) {
         return userService.findUserById(id)
                 .map(user -> {
                     model.addAttribute("user", user);
+                    model.addAttribute("roles", Role.values());
+                    model.addAttribute("companies", companyService.findAll());
                     return "user/user";
                 }).orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND));
     }
 
     @PostMapping
-    public String createUser(@ModelAttribute("user") UserCreateEditDto user) {
+    public String createUser(@ModelAttribute UserCreateEditDto user, RedirectAttributes redirectAttributes) {
+        if (true) {
+//            redirectAttributes.addAttribute("username", user.getUsername());
+//            redirectAttributes.addAttribute("firstname", user.getFirstname());
+            redirectAttributes.addFlashAttribute("user", user);
+            return "redirect:/users/registration";
+        }
         UserReadDto dto = userService.createUser(user);
         return "redirect:/users/" + dto.getId();
     }
--- src/main/java/com/abit/spring/service/CompanyService.java
+++ src/main/java/com/abit/spring/service/CompanyService.java
@@ -1,10 +1,25 @@
 package com.abit.spring.service;
 
+import com.abit.spring.database.repository.CompanyRepository;
+import com.abit.spring.dto.CompanyDto;
+import com.abit.spring.mapper.CompanyReadMapper;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.util.List;
+
 @Service
-@Transactional
+@Transactional(readOnly = true)
+@RequiredArgsConstructor
 public class CompanyService {
 
+    private final CompanyRepository companyRepository;
+    private final CompanyReadMapper companyReadMapper;
+
+    public List<CompanyDto> findAll() {
+        return companyRepository.findAll().stream()
+                .map(companyReadMapper::map)
+                .toList();
+    }
 }
